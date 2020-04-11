70d9fc4afcca3c89b6a1dc421a9ed1c23dcd8b08
Faizan Ahmed Siddiqui <39314095+Faizi-alpha@users.noreply.github.com>
2020-04-12 00:08:54 +0530

    Added Comments

--- LinkedList/Pairwise Swap in a LinkedList.java
+++ LinkedList/Pairwise Swap in a LinkedList.java
@@ -1,12 +1,11 @@
 // Problem Link :-https://practice.geeksforgeeks.org/problems/pairwise-swap-of-nodes-in-linkelist/1/?track=PC-W5-LL&batchId=189
 
 
-// Function for the problems
+// Function for the problem
 
 
  public static Node pairwise_swap(Node node)
     {
-        // your code here
         
         // This is just for swapping the first two nodes of linkedlist
         Node n1 = node.next.next;
@@ -17,11 +16,9 @@
         // Now we swap the remaining of linkedlist
         while(n1!=null && n1.next!=null)
         {
-            prev.next = n1.next;https://practice.geeksforgeeks.org/problems/pairwise-swap-of-nodes-in-linkelist/1/?track=PC-W5-LL&batchId=189
+            prev.next = n1.next;
             prev = n1;
-
-           
-            Node nxt = n1.next.next;
+            Node nxt = n1.next.next;  // New node so that we dont loose the next node which right after the element whose refrence we are changing.
             n1.next.next = prev;
             n1 = nxt;
             
