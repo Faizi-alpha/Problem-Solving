05a1c16a903f2f61ddd67e43939d07d2857ea03d
Faizan Ahmed Siddiqui <39314095+Faizi-alpha@users.noreply.github.com>
2020-06-06 11:38:38 +0530

    Create MinheapClass.java

---
+++ Binary Heap/MinheapClass.java
@@ -0,0 +1,35 @@
+/* We need
+1. An array for storing heap elements
+2. size variable (integer)
+3. capacity variable (integer)
+
+Class MinheapClass
+{ 
+   int arr[];
+   int size;
+   int cap;
+   
+   MinheapClass(int c)
+   {
+    arr = new int[c];
+    size = 0;
+    capacity = c;
+   }
+   
+   int left(int i)    // this is helpful in getting left of root using array index
+   {
+     return (2*i+1);
+   }
+   
+   int right(int i)  // this is helpful in getting right of root using array index
+   {
+    return (2*i+2);
+   }
+   
+   int parent(int i)  // similarly, for parent
+   {
+     Math.floor((i-1)/2);
+   }
+   
+   
+}
