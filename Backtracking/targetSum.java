297f77654f4e38d4ef14bfd5c2260717021360f5
Faizan Ahmed Siddiqui <39314095+Faizi-alpha@users.noreply.github.com>
2020-12-12 15:32:04 +0530

    Create targetSum.java

---
+++ Backtracking/targetSum.java
@@ -0,0 +1,41 @@
+	
+// Target Sum
+// Problem Link : https://www.youtube.com/watch?v=zNxDJJW40_k&list=PL-Jc9J83PIiFxaBahjslhBD1LiJAV7nKs&index=48
+  
+import java.io.*;
+import java.util.*;
+
+
+class targetSum{
+	public static void main(String args[]) throws IOException {
+		
+//	    Use this for user input.
+//	    Scanner sc = new Scanner(System.in);
+
+		int arr[] = {10,2,1,5,4,25,9,12,8};
+		tSum(arr,0,"",0,9);
+		
+	}
+  
+  // sos is sum of subset
+  // set is subset
+  // idx is index
+  // tar is target
+
+	public static void tSum(int[] arr, int idx, String set, int sos, int tar)
+	{
+		if(sos == tar)
+		{
+			System.out.println(set);
+			return;
+		}
+		
+		if(idx == arr.length || sos > tar)
+			return;
+		
+		
+		String curr = String.valueOf(arr[idx]);
+		tSum(arr,idx+1,set+" "+curr,sos+arr[idx],tar);
+		tSum(arr,idx+1,set,sos,tar);
+	}
+}
